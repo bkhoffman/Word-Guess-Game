9597bed68acc9d5f5c8fb9f57760a388eac7d9c8
boffman <bkhoffman@gmail.com>
2019-03-07 13:21:13 -0600

    updates to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //variables
 var movieTitles = ["avatar", "titanic", "avengers", "rocky", "inception"]
-var alphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var wins = 0;
 var movieName; //hold the randomly choosen movie title
 var movieNameArr = []; //holds "_", add guessed letters to this array if correct
@@ -30,17 +30,15 @@ function setup(){
       guesses = 14;
     }
   //empty movie array. Will hold "_" places
-  movieNameArr = []; //do i need this? will the var array declaration above empty for every reset?
+  movieNameArr = [];
   //iterates through "picked" movie title and fill in with "_"
   for (var i = 0; i < movieName.length; i++) {
     movieNameArr[i] = "_";
-    console.log(movieNameArr);
   }
 
-  //reset vars
-  //guessesRemaining = guesses;
-  guessedLetters = [];  //resets array to blank
-
+  //reset vars and empty array
+  guessesRemaining = guesses;
+  guessedLetters = [];
 }
 setup();
 
@@ -50,41 +48,41 @@ setup();
 document.onkeyup = function(event){
   var userGuess = event.key.toLocaleLowerCase();
   console.log(userGuess);
-  if(userGuess === "a"){
-    guessedLetters.push(userGuess);
-    console.log(guessedLetters);
-  }
-  
+  if (event.keyCode >= 65 && event.keyCode <= 90){
     for (var i = 0; i < movieName.length; i++) {
       if (userGuess === movieName[i]) {
-          movieNameArr[i] = userGuess;
+        //movieNameArr[i] = userGuess;
+        console.log("correct");
       }
     }
+  }
     
-  
+  // if(guessedLetters.indexOf(userGuess) === -1){
+  //   guessedLetters.push(userGuess);
+  //   guesses--;
+  //   console.log(movieNameArr);
+  //   }else {
+  //     for (var i = 0; i < movieName.length; i++) {
+  //       if (userGuess === movieName[i]) {
+  //         movieNameArr[i] = userGuess;
+  //         console.log(movieNameArr);
+  //       }
+  //   }
+  // } 
+  // if(movieNameArr.indexOf(userGuess) !== -1){}
+
+  //shows vars/arrays on html by id name
   document.getElementById("guessedLetters").innerText = guessedLetters;
-  // if(userGuess !== movieNameArr[i]){
-  //   console.log("wrong");
-  // }
+  document.getElementById("numberGuess").innerText = guesses;
+  document.getElementById("numberOfWins").innerText = wins;
+  document.getElementById("movieName").innerText = movieNameArr.join(' ');
 }
-//shows vars/arrays on html by id name
-document.getElementById("numberGuess").innerText = guesses;
-
-document.getElementById("numberOfWins").innerText = wins;
-document.getElementById("movieName").innerText = movieNameArr.join(' ');
-   
   
-
-// if(event.keyCode >= 65 && event.keyCode <= 90) {
-//   guessChecker(event.key.toLowerCase()); 
-//   //updateScreen();
-//   //isWinner();
-//   //isLoser();
-// function guessChecker(userGuess){
-//   for (var i = 0; i < movieName.length; i++) {
-//     if (userGuess === movieName[i]) {
-//         movieNameArr[i] = userGuess;
-//     }
-//   }
-// };
+  // We can also do this with a function. zoo animal example
+  // function guessChecker(letterGuess) {
+  //   for (var j = 0; j < letterGuess.length; j++) {
+  //     console.log(letterGuess[j]);
+  //   }
+  // }
+  // guessChecker(userGuess); //passing event key into guessChecker function instead of assigning to var and then passing to guessChecker function
   
